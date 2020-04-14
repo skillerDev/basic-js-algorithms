ab2f6642f52d1e02f7a1cd258602f95a606c4a16
GChristians <skillerDev@Ginas-MBP.hsd1.co.comcast.net>
2020-04-14 12:24:47 -0600

    solving katas day 1

---
+++ veryEasyKatas.js
@@ -0,0 +1,26 @@
+// convert minutes to seconds 
+// 1. write a fn that takes an integer in minutes and converts it to seconds
+
+const convert = minutes => {
+    return minutes * 60
+}
+
+// 2. squaring a number **use Math.pow()**
+
+const squared = b => Math.pow(b,2)
+
+//  3. Return the Sum of Two Numbers; Create a function that takes
+//  two numbers as arguments and return their sum
+
+const addition = (a,b) => a +b
+
+// 4. Return the First Element in an Array
+// Create a function that takes an array and returns the first element.
+
+const getFirstValue = arr => arr[0]
+
+// 5. Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, 
+// increments the number by +1 and returns the result.
+
+
